1662794459a709caa4d7ed28bb187f39b2a9b0a9
Kamil Cie�lak <keraxel@gmail.com>
2013-06-01 01:02:48 +0200

    Init commit

---
+++ HashGenerator/SplitPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using HashGenerator.Common;
+
+using Windows.UI.Xaml.Controls;
+
+namespace HashGenerator
+{
+    /// <summary>
+    /// A page that displays a group title, a list of items within the group, and details for the
+    /// currently selected item.
+    /// </summary>
+    public sealed partial class SplitPage : LayoutAwarePage
+    {
+        public SplitPage()
+        {
+            InitializeComponent();
+
+            DataContext = new ViewModel();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            (DataContext as ViewModel).Text = ((TextBox) sender).Text;
+        }
+    }
+}
---
+++ HashGenerator/ViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using HashGenerator.Annotations;
+
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage.Streams;
+
+namespace HashGenerator
+{
+    public sealed class ViewModel : INotifyPropertyChanged
+    {
+        private string text;
+        private string hash;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+
+            set
+            {
+                text = value;
+                Hash = ComputeHash(text, "MD5");
+                OnPropertyChanged();
+            }
+        }
+
+        public string Hash
+        {
+            get
+            {
+                return hash;
+            }
+
+            set
+            {
+                hash = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private static string ComputeHash(string str, string hashType)
+        {
+            var alg = HashAlgorithmProvider.OpenAlgorithm(hashType);
+            IBuffer buff = CryptographicBuffer.ConvertStringToBinary(str, BinaryStringEncoding.Utf8);
+            var hashed = alg.HashData(buff);
+            var res = CryptographicBuffer.EncodeToHexString(hashed);
+            return res;
+        }
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
